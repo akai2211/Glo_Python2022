ea7cc8a79947c2e3af8b8adda040e707abdc1e5f
Akai <wipmoon@gmail.com>
2022-02-26 01:49:28 +0700

    Update Mission 1.py
    Добавлены правки куратора

--- Lesson 3/Mission 1.py
+++ Lesson 3/Mission 1.py
@@ -2,4 +2,13 @@ print("Glo", end="")
 print(" ", end="")
 print("Academy")
 print()
+
+print("Glo Academy")
+print()
+
+print("Glo", end="")
+print(" ", end="")
+print("Academy")
+print()
+
 print("Glo Academy")
